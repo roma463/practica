bd2a35f23fb6ba1d022deece55244e6b1cae4692
roma463 <461noname.error4o4@gmail.com>
2023-04-04 22:47:07 +0700

    new

---
+++ Pracrica_Atelie/CreateOrder.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Pracrica_Atelie.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pracrica_Atelie
+{
+
+    /// <summary>
+    /// Логика взаимодействия для CreateOrder.xaml
+    /// </summary>
+    public partial class CreateOrder : Window
+    {
+        private Grid _mainWindow;
+        private int _currentEmployee;
+        public CreateOrder(Grid mainWindow, int idEmployee)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+            _currentEmployee = idEmployee;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Order newOrder = new Order();
+            newOrder.TimeOfOrder = DateTime.Parse(timeBegin.Text.ToString());
+            newOrder.EndTime = DateTime.Parse(TimeEnd.Text.ToString());
+            newOrder.IdEmployee = _currentEmployee;
+            newOrder.IdClient = int.Parse(Client.Text);
+            newOrder.IdTitleWork = Disain.IsChecked == true ? 4 : 1;
+            newOrder.IdMaterials = int.Parse(Matetials.Text);
+            _mainWindow.Create(newOrder);
+            this.Close();
+        }
+    }
+}
---
+++ Pracrica_Atelie/Grid.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Pracrica_Atelie.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pracrica_Atelie
+{
+    /// <summary>
+    /// Логика взаимодействия для Grid.xaml
+    /// </summary>
+    public partial class Grid : Window
+    {
+        Entities db = Entities.GetContext();
+        private int _currentMaster = 3;
+        public Grid()
+        {
+            refresh();
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выбранный элемент
+            Order selectedItem = dtg_orders.SelectedItem as Order;
+
+            if (selectedItem != null)
+            {
+
+                // получаем DbSet для таблицы, содержащей элементы
+                DbSet<Order> items = db.Order;
+
+                // удаляем элемент из DbSet
+                items.Remove(selectedItem);
+
+                // сохраняем изменения в базе данных
+                db.SaveChanges();
+                refresh();
+            }
+        }
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            CreateOrder create = new CreateOrder(this, _currentMaster);
+            create.Show();
+        }
+
+        public void Create(Order Order)
+        {
+            db.Order.Add(Order);
+            db.SaveChanges();
+            refresh();
+        }
+
+        private void btn_red_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выбранный элемент
+            Order selectedItem = dtg_orders.SelectedItem as Order;
+
+            if (selectedItem != null)
+            {
+                db.Entry(selectedItem).State = EntityState.Modified;
+                // сохраняем изменения в базе данных
+                db.SaveChanges();
+                refresh();
+            }
+        }
+        public void refresh()
+        {
+            InitializeComponent();
+            var querry = db.Order;
+            dtg_orders.ItemsSource = querry.ToList();
+        }
+    }
+}
